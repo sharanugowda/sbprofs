aa2e40956e89b045ea40416dd83b41858803468e
looser88 <98893135+looser88@users.noreply.github.com>
2022-03-07 09:08:43 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -38,7 +38,7 @@ async def save_group(bot, message):
             return
         buttons = [[
             InlineKeyboardButton('Help', url=f"https://t.me/{temp.U_NAME}?start=help"),
-            InlineKeyboardButton('Updates', url='https://t.me/Groupdc')
+            InlineKeyboardButton('Updates', url='https://t.me/Dot_serials_bot')
         ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await message.reply_text(
