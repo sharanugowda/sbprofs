eae71b3b8ba50ff95011a6c6cd274f5fb430e97b
looser88 <98893135+looser88@users.noreply.github.com>
2022-03-07 21:50:06 +0530

    Update genlink.py

--- plugins/genlink.py
+++ plugins/genlink.py
@@ -34,7 +34,7 @@ async def gen_link_s(bot, message):
     string = 'filep_' if message.text.lower().strip() == "/plink" else 'file_'
     string += file_id
     outstr = base64.urlsafe_b64encode(string.encode("ascii")).decode().strip("=")
-    await message.reply(f"🚀 Here is your Link:\nhttps://t.me/{temp.U_NAME}?start={outstr}")
+    await message.reply(f"🚀 Here is your Link:\n\nhttps://t.me/{temp.U_NAME}?start={outstr}, ```https://t.me/{temp.U_NAME}?start={outstr}```")
     
     
 @Client.on_message(filters.command(['batch', 'pbatch']) & filters.create(allowed))
