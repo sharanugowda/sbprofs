e63a2b10e30c0ff643d43295bd5a313c0fd8f8cd
looser88 <98893135+looser88@users.noreply.github.com>
2022-03-10 08:34:35 +0530

    Update genlink.py

--- plugins/genlink.py
+++ plugins/genlink.py
@@ -20,16 +20,16 @@ async def allowed(_, __, message):
         return True
     return False
 
-@Client.on_message(filters.command(['link', 'plink']) & filters.create(allowed))
+@Client.on_message(filters.command(['cheesylink', 'plink']) & filters.create(allowed))
 async def gen_link_s(bot, message):
     replied = message.reply_to_message
     if not replied:
         return await message.reply('Reply to a message to get a shareable link.')
     file_type = replied.media
-    if file_type not in ["video", 'audio', 'document']:
+    if file_type not in ["video",'photo', 'audio', 'document']:
         return await message.reply("Reply to a supported media")
     if message.has_protected_content and message.chat.id not in ADMINS:
-        return await message.reply("okDa")
+        return await message.reply("okDear")
     file_id, ref = unpack_new_file_id((getattr(replied, file_type)).file_id)
     string = 'filep_' if message.text.lower().strip() == "/plink" else 'file_'
     string += file_id
@@ -37,13 +37,13 @@ async def gen_link_s(bot, message):
     await message.reply(f"🚀 Here is your Link:\n\nhttps://t.me/{temp.U_NAME}?start={outstr} \n\n```https://t.me/{temp.U_NAME}?start={outstr}```\n```https://t.me/{temp.U_NAME}?start={outstr}```")
     
     
-@Client.on_message(filters.command(['batch', 'pbatch']) & filters.create(allowed))
+@Client.on_message(filters.command(['cheesybatch', 'pbatch']) & filters.create(allowed))
 async def gen_link_batch(bot, message):
     if " " not in message.text:
-        return await message.reply("Use correct format.\nExample <code>/batch https://t.me/umlinks/10 https://t.me/umlinks/20</code>.")
+        return await message.reply("Use correct format.\nExample <code>/batch https://t.me/********/10 https://t.me/********/20</code>.")
     links = message.text.strip().split(" ")
     if len(links) != 3:
-        return await message.reply("Use correct format.\nExample <code>/batch https://t.me/umlinks/10 https://t.me/umlinks/20</code>.")
+        return await message.reply("Use correct format.\nExample <code>/batch https://t.me/********/10 https://t.me/********/20</code>.")
     cmd, first, last = links
     regex = re.compile("(https://)?(t\.me/|telegram\.me/|telegram\.dog/)(c/)?(\d+|[a-zA-Z_0-9]+)/(\d+)$")
     match = regex.match(first)
