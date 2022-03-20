ec0877e0681757ebe8cb2d474a3bbff60b559feb
looser88 <98893135+looser88@users.noreply.github.com>
2022-03-20 09:09:22 +0530

    Update genlink.py

--- plugins/genlink.py
+++ plugins/genlink.py
@@ -34,7 +34,7 @@ async def gen_link_s(bot, message):
     string = 'filep_' if message.text.lower().strip() == "/plink" else 'file_'
     string += file_id
     outstr = base64.urlsafe_b64encode(string.encode("ascii")).decode().strip("=")
-    await message.reply(f"🚀 Here is your Link:\n\nhttps://telegram.me/{temp.U_NAME}?start={outstr} \n\n```https://t.me/{temp.U_NAME}?start={outstr}```\n```https://t.me/{temp.U_NAME}?start={outstr}```")
+    await message.reply(f"🚀 Here is your Link:\n\nhttps://telegram.me/{temp.U_NAME}?start={outstr} \n\n```https://telegram.me/{temp.U_NAME}?start={outstr}```\n```https://telegram.me/{temp.U_NAME}?start={outstr}```")
     
     
 @Client.on_message(filters.command(['cheesybatch', 'pbatch']) & filters.create(allowed))
